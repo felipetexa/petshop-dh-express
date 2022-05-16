148a2426f9a7ff4d5be3bf941e5d5543bad7abac
felipetexa <felipe.teixeira91@hotmail.com>
2022-05-16 15:30:43 -0300

    adicionando rota de produtos

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const petsRouter = require('./routes/petRouter');
+const productRouter = require('./routes/productRouter')
 
 app.use(petsRouter);
+app.use(productRouter);
 
 app.get('/', (req, res) => {
   res.send('Olá mundo');
