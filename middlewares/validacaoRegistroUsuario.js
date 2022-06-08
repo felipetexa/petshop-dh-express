93d6478fa13038337c4f68f873b8ecd1ed7965c0
felipetexa <felipe.teixeira91@hotmail.com>
2022-06-08 16:55:07 -0300

    desafio - confirmação de email via express validator

--- middlewares/validacaoRegistroUsuario.js
+++ middlewares/validacaoRegistroUsuario.js
@@ -4,6 +4,12 @@ const validacaoRegistroUsuario = [
     body("nome")
         .notEmpty().withMessage("Deve preencher o nome").bail()
         .isLength({min: 3, max: 200}).withMessage("O nome deve ter no minimo 3 caracteres"),
+    body("email")
+        .notEmpty().withMessage("Deve preencher o email").bail()
+        .isEmail().withMessage("Deve ser um email válido"),
+    body("senha")
+        .notEmpty().withMessage("Deve preencher a senha").bail(),
+        
 ]
 
 module.exports = validacaoRegistroUsuario;
